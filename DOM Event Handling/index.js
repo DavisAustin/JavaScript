b1c026f86495e659407be3d2d382d703ce26d3a5
Austin Davis <Austin.DavisDesigns@Gmail.Com>
2019-07-09 14:05:57 -0700

    Event Listeners
    Learing how to utilize event listeners to interact with the webpage.

---
+++ DOM Event Handling/index.js
@@ -0,0 +1,23 @@
+// MDM Event Reference for Event References
+
+// Common Browser-Level Events
+
+// Load - when the resource and dependents have finished loading
+// Error - when a resource failed to load
+// Online/offline - self-explanitory
+// Resize - when a viewport is resized
+// Scroll - when a viewport is scrolled up/down/left/right
+
+// Common DOM Events
+
+// Focus - when an element receives focus (clicked, tabbed toString, etc.)
+// Blur - when an element loses focus (leaving form field, etc.)
+// Resets/submit - form-specific events
+// Mouse Events - click, mouseover, drag, drop, etc.
+
+// Other Events
+
+// Media Events - relate to audio/video playback
+// Progress Events - monitor ongoing browser progress
+// CSS Transition Events - transition start/run/end
+
