c9f8c0ba17f1382905b47cb946c7edc9571eeb71
Austin Davis <Austin.DavisDesigns@Gmail.Com>
2019-07-09 22:47:58 -0700

    Typing Game Chapter 08_02
    Working through a typing game tutorial

--- Ex_Files_JavaScript_EssT/Exercise Files/08_02/script.js
+++ Ex_Files_JavaScript_EssT/Exercise Files/08_02/script.js
@@ -4,20 +4,38 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
+var timer = [0,0,0,0];
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 
 
 // Run a standard minute/second/hundredths timer:
-
+function runTimer() {
+    let currentTimer = timer[0] + ":" + timer[1] + ":"
+    theTimer.innerHTML = timer;
+    timer[3]++;
+}
 
 // Match the text entered with the provided text on the page:
+function spellCheck() {
+    let textEntered = testArea.value;
 
+}
 
 // Start the timer:
-
+function start() {
+    let textEnteredLength = testArea.nodeValue.length;
+    if (textEnteredLength === 0) {
+        setInterval(runTimer, 10);
+    }
+}
 
 // Reset everything:
+function reset() {
 
+}
 
 // Event listeners for keyboard input and the reset button:
+testArea.addEventListener("keypress", start, false);
+testArea.addEventListener("keyup", spellCheck, false);
+resetButton.addEventListener("click", start, false);
