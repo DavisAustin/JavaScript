c6939f5a3b0712265e7aa1a385cafe3105c76d76
Austin Davis <Austin.DavisDesigns@Gmail.Com>
2019-07-14 15:08:24 -0700

    Equation game
    textarea value is cleared on blur

--- Equation Game/index.js
+++ Equation Game/index.js
@@ -1,97 +1,46 @@
-// // GLOBALLY SCOPED VARIABLES
-// const firstNumber = 10;
-// const secondNumber = 13;
-
-// // Immediately invoked function
-// let multiplyThese = (function(a,b) {
-//     // Locally scoped variable
-//     let result = a * b;
-//     return result;
-// })(firstNumber,secondNumber);
-
-// console.log(multiplyThese);
-
-
-// // Creating Objects
-// function MyToolBox(hammer, drill, saw, screwdriver) {
-//   this.hammer = hammer,
-//   this.drill = drill,
-//   this.saw = saw,
-//   this.screwdriver = screwdriver
-// };
-
-// var myTool = new MyToolBox("The hammer", "My drill", "My saw", "My screwdriver");
-
-// console.log(myTool);
-
-//More Object Practice
-// function Person(name, age, favoriteColor) {
-//   this.name = name,
-//   this.age = age,
-//   this.favoriteColor = favoriteColor
-// };
-
-// var newPerson = [
-//   person01 = new Person("Jake", 12, "Blue"),
-//   person02 = new Person("Heather", 24, "Aquamarine"),
-//   person03 = new Person("Noah", 31, "Paleo Blue"),
-//   person04 = new Person("Vicky", 29, "Ruby")
-// ];
-
-// console.log(newPerson[1]);
-// console.log(newPerson[2]);
-// console.log(newPerson[0]);
-// console.log(newPerson[3]);
-
-// const TIMER = document.querySelector("#timer");
-// TIMER.innerHTML = "Hello World!";
-
-
-// //NAME GENERATOR
-// const FIRSTNAMES = ["Amy", "Brian", "Cody", "Dennis", "Ellen", "Fredrick", "Ginger", "Heather", "Izzy", "James"];
-
-// const LASTNAMES = ["Anderson", "Brady", "Collins", "Davis", "Eisen", "Foster", "Gordon", "Hamelton", "Isawa", "Jenson"];
-
-
-// function nameGenerator() {
-//     return "My name is " + pickFirstName() + " " + pickLastName() + ".";
-// };
-
-// function randomNumber() {
-//   return Math.floor(Math.random()*10);
-// };
-
-// function pickFirstName() {
-//   return FIRSTNAMES[randomNumber()];
-// };
-
-// function pickLastName() {
-//   return LASTNAMES[randomNumber()];
-// };
-
-// console.log(nameGenerator());
-
 const EQUATION = document.querySelector("#equation");
 const ANSWER = document.querySelector("#answer");
 
+let gameRunning = false;
+let answer;
+// Can text area be numerical value only?
+let userAnswer = ANSWER.value;
+
 // Check if answer is equal to equation when user presses enter
 function checkAnswer() {
-};
+    console.log(answer);
+}
 
 // Create an addition equation
-function createAdditionEquation() {
-    var a = randomNumber();
+function generateEquation() {
+    let a = randomNumber();
         b = randomNumber();
     EQUATION.innerHTML = a + " + " + b;
-    var answer = a + b;
+    let answer = a + b;
     return answer;
-};
+}
 
 // Pick a random number between 1 and 10
 function randomNumber() {
-    var randomNumber = Math.floor(Math.random()*11);
-    if (randomNumber == 0) {
-        randomNumber++;
-    };
+    let randomNumber = Math.floor(Math.random()*10 + 1);
     return randomNumber;
-};
+}
+
+// Add placeholder text to textarea
+function addPlaceholder() {
+    console.log("Add Placeholder");
+    ANSWER.setAttribute("placeholder", "Answer here...");
+}
+
+// Remove placeholder text from textarea
+function removePlaceholder() {
+    console.log("Remove Placeholder");
+    ANSWER.value = "";
+    ANSWER.setAttribute("placeholder", "");
+}
+
+// Event listeners
+ANSWER.addEventListener("focus", removePlaceholder, false);
+ANSWER.addEventListener("blur", addPlaceholder, false);
+ANSWER.addEventListener("click", generateEquation, false);
+ANSWER.addEventListener("keyup", checkAnswer, false);
--- Simple Timer/index.js
+++ Simple Timer/index.js
@@ -1,20 +1,51 @@
+// Select DOM Elements
 const THETIMER = document.querySelector("#timer");
 const THEBUTTON = document.querySelector("#start-stop");
+const RESET = document.querySelector("#reset");
 
 var timer = 0;
 var interval;
+var timerRunning = false;
 
+// Increases the timer variable incrementally by 1
 function countUp() {
+    // Add system milliseconds and let it out
     timer++;
     THETIMER.innerHTML = timer;
-    if (timer === 10) {
-        clearInterval(interval);
+}
+
+// Start the timer
+function startTimer() {
+    interval = setInterval(countUp, 1000);
+    THEBUTTON.innerHTML = "Stop";
+}
+
+// Stop the timer
+function stopTimer() {
+    clearInterval(interval);
+    THEBUTTON.innerHTML = "Start";
+}
+
+// Sets the interval for countUp
+function toggleTimer() {
+    if (timerRunning){
+        stopTimer();
+        timerRunning = false;
+    } else {
+        startTimer();
+        timerRunning = true;
     }
 }
 
-function startTimer() {
-    interval = setInterval(countUp, 1000);
+// Clears interval and resets timer to 0
+function resetTimer() {
+    clearInterval(interval);
+    THETIMER.innerHTML = "0";
+    timer = 0;
+    timerRunning = false;
 }
 
 
-THEBUTTON.addEventListener("click", startTimer, false);
+// Event Listeners
+THEBUTTON.addEventListener("click", toggleTimer, false);
+RESET.addEventListener("click", resetTimer, false);
