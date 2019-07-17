034b2da49a5b3460eadd8930906dd5dfbbb6ef89
Austin Davis <Austin.DavisDesigns@Gmail.Com>
2019-07-16 23:58:43 -0700

    Check Answer
    Check answer functionality almost complete! Needs an incorrect feature that fires after a user inputs their proposed answer.

--- Equation Game/index.js
+++ Equation Game/index.js
@@ -3,22 +3,43 @@ const ANSWER = document.querySelector("#answer");
 
 let gameRunning = false;
 // Can text area be numerical value only?
-let userAnswer = ANSWER.value;
-let answer = generateEquation();
+let userAnswer;
+let equationAnswer;
+let currentEquation;
 
 // Check if answer is equal to equation when user presses enter
 function checkAnswer() {
-    console.log("Checking Answer");
-    console.log(answer);
+    userAnswer = ANSWER.value;
+    if (equationAnswer == userAnswer) {
+        console.log("Correct!");
+        ANSWER.classList.replace("focused", "correct");
+        EQUATION.innerHTML = "Correct!"
+        setTimeout(function() {
+            console.log(equationAnswer);
+            ANSWER.value = "";
+            ANSWER.classList.replace("correct", "focused");
+            generateEquation();
+        }, 500);
+    } else if (userAnswer > equationAnswer) {
+        console.log("Incorrect!");
+        EQUATION.innerHTML = "Incorrect!"
+        ANSWER.classList.replace("focused", "incorrect");
+        setTimeout(function() {
+            ANSWER.value = "";
+            EQUATION.innerHTML = currentEquation;
+            ANSWER.classList.replace("incorrect", "focused");
+        }, 500);
+    }
 }
 
 // Create an addition equation
 function generateEquation() {
     let a = randomNumber();
         b = randomNumber();
-    EQUATION.innerHTML = a + " + " + b;
-    var equationSolution = a + b;
-    return equationSolution;
+    currentEquation = a + " + " + b;
+    EQUATION.innerHTML = currentEquation;
+    equationAnswer = a + b;
+    return equationAnswer;
 }
 
 // Pick a random number between 1 and 10
@@ -31,6 +52,7 @@ function randomNumber() {
 function addPlaceholder() {
     console.log("Add Placeholder");
     ANSWER.setAttribute("placeholder", "Answer here...");
+    ANSWER.classList.replace("focused", "neutral");
 }
 
 // Remove placeholder text from textarea
@@ -38,6 +60,8 @@ function removePlaceholder() {
     console.log("Remove Placeholder");
     ANSWER.value = "";
     ANSWER.setAttribute("placeholder", "");
+    ANSWER.classList.replace("neutral", "focused");
+    console.log(ANSWER.classList);
 }
 
 // Event listeners
