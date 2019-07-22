9d529a96a86a2691ce1a1b019a5e1039aeb0adb9
Austin Davis <Austin.DavisDesigns@Gmail.Com>
2019-07-22 15:05:08 -0700

    subString Methods
    Learning how to use subString methods in order to check answers in the equation game.

---
+++ Substring Test/index.js
@@ -0,0 +1,5 @@
+const SAMPLETEXT = document.querySelector("#sample-text");
+const USERTEXT = document.querySelector("#user-text");
+
+console.log(SAMPLETEXT.innerHTML);
+console.log(USERTEXT.value);
