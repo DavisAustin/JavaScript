83f98e6a2ba0f117d49af7c628fc041bf9ffd1e1
Austin Davis <Austin.DavisDesigns@Gmail.Com>
2019-07-22 17:52:59 -0700

    Substring Test
    Substring test is working! Now it can be implemented in the Equation Game! :D

--- Substring Test/index.js
+++ Substring Test/index.js
@@ -1,5 +1,25 @@
 const SAMPLETEXT = document.querySelector("#sample-text");
 const USERTEXT = document.querySelector("#user-text");
 
-console.log(SAMPLETEXT.innerHTML);
-console.log(USERTEXT.value);
+let userInput = USERTEXT.value;
+let sampleTextHtml = SAMPLETEXT.innerHTML;
+
+function stringLength() {
+    let textEntered = USERTEXT.value;
+    let sampleTextMatch = sampleTextHtml.substring(0,textEntered.length);
+
+    if (textEntered == sampleTextHtml) {
+        console.log("Correct!");
+    } else {
+        if (textEntered == sampleTextMatch) {
+            USERTEXT.style.borderColor = "#65CCf3";
+            console.log("Neutral");
+        } else {
+            USERTEXT.style.borderColor = "#E95D0F";
+            console.log("Wrong!");
+        }
+    }
+
+}
+
+USERTEXT.addEventListener("keyup", stringLength, false);
